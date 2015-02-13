4da0103ae60a8a7ce34c3c2f6f1749d50cd03d32
Ian Mankowski <ltethe@gmail.com>
2015-02-13 02:14:28 -0800

    gui placement
    gui elements now show up relative to player.

--- Assets/scripts/playerState.cs
+++ Assets/scripts/playerState.cs
@@ -32,6 +32,11 @@ public class playerState : MonoBehaviour {
 	gameCenter gameCenterScript;
 	resetCamera resetCameraScript;
 
+	GameObject targetingPanel_A;
+	CanvasGroup targetingPanel_A_CanvasGroup;
+
+	GameObject targetingPanel_B;
+	CanvasGroup targetingPanel_B_CanvasGroup;
 
 	//This bit here turns this into a singleton
 	private static playerState _instance;
@@ -61,6 +66,9 @@ public class playerState : MonoBehaviour {
 		player2 = !player2;	
 
 		resetCameraScript.smoothResetCamera();
+
+		targetingPanel_A_CanvasGroup.alpha = 0;
+		targetingPanel_B_CanvasGroup.alpha = 0;
 		
 		///Simply keeps track of how many turns have taken place///
 		if (player1 == true)
@@ -68,12 +76,14 @@ public class playerState : MonoBehaviour {
 			player1Stats += 1;
 			playerObject.collider2D.enabled = true;				//enable user input for opposite player
 			gameCenterScript.updateCenterOfInterest(shipA);
+			targetingPanel_A_CanvasGroup.alpha = 1;
 		}
 		else if (player2 == true)
 		{
 			player1Stats += 1;
 			playerObject2.collider2D.enabled = true;			//enable user input for opposite player
 			gameCenterScript.updateCenterOfInterest(shipB);
+			targetingPanel_B_CanvasGroup.alpha = 1;
 
 		}
 
@@ -108,6 +118,13 @@ public class playerState : MonoBehaviour {
 
 	public void Start()
 	{
+		targetingPanel_A = GameObject.Find("targetingText_A_panel");
+		targetingPanel_A_CanvasGroup = targetingPanel_A.GetComponent<CanvasGroup>();
+
+
+		targetingPanel_B = GameObject.Find("targetingText_B_panel");
+		targetingPanel_B_CanvasGroup = targetingPanel_B.GetComponent<CanvasGroup>();
+
 		actionCenter = GameObject.Find("actionCenter");
 		gameCenterScript = actionCenter.GetComponent<gameCenter>();
 		resetCameraScript = Camera.main.GetComponent<resetCamera>();
