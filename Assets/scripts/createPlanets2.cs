420938ffdb0821131bc7b503dc9ae3e36f8a8d7f
Ian Mankowski <ltethe@gmail.com>
2015-04-15 20:02:30 -0700

    child planet
    destroy planet creates child planet

---
+++ Assets/scripts/createPlanets2.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createPlanets2 : MonoBehaviour {
+
+	Vector2 worldPos;
+	public float planetScale;
+
+	public int lowRange = 1;
+	public int highRange = 3;
+	public int numberOfPieces;
+
+	public GameObject planet;
+	public GameObject planetGroup;
+	public GameObject gameManager;
+
+
+	//void planetCreate(Vector2 worldPos, float planetScale)
+	public void planetCreate()
+	{
+		for (int i = 0; i < numberOfPieces; i++)
+		{
+
+			/*
+			//place the other planets, use collision detection so no planets overlap.
+			while (Physics2D.OverlapCircle(worldPos, planetScale)){
+				position = new Vector2((worldPos + Random.Range(0.0f, planetScale), (worldPos + Random.Range(0.0f, planetScale)));
+				//placeData (position, out worldPos, out planetScale);
+			}
+			*/
+			//instantiate a planet
+			planet = Instantiate(planet, worldPos, Quaternion.identity) as GameObject;
+			planet.name = "planet_";
+			//planet.name = "planet_" + planetEnumerator.ToString();
+
+			planet.GetComponent<health>().healthAmount = (int)(planetScale * 5);
+
+			//Apply scale
+			
+			planet.transform.localScale = Vector3.one * planetScale;
+			planet.transform.parent = planetGroup.transform;
+			planetGroup.transform.parent = gameManager.transform;
+		}
+		
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		numberOfPieces = Random.Range(lowRange, highRange);	
+		worldPos = transform.position;
+		planetScale = transform.localScale.x/(numberOfPieces+1);
+
+		gameManager = GameObject.FindWithTag("gameManager");
+		planetGroup = GameObject.Find("planetGroup");
+	}
+}
--- Assets/scripts/health.cs
+++ Assets/scripts/health.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class health : MonoBehaviour {
 
 	public int healthAmount = 3;
+	createPlanets2 createChildren;
 
 	public void damage()
 	{
 		healthAmount -= 1;
 		if (healthAmount == 0)
 		{
+			createChildren.planetCreate();
 			Destroy(gameObject);
+
 		}
 		else if (healthAmount <= -1)
 		{
@@ -24,6 +27,7 @@ public class health : MonoBehaviour {
 		{
 			healthAmount = -1;
 		}
+		createChildren = gameObject.GetComponent<createPlanets2>();
 	}
 }
 
