6e89e0f2284f64a3d7289ba76b61db381903e65c
Ian Mankowski <ltethe@gmail.com>
2015-10-21 00:19:08 -0700

    Implemented Version Editor

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.Scripts.VersionEditor;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]
+	private Text versionText;
+
+	// Use this for initialization
+	void Start () {
+		versionText.text = VersionInformation.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/VersionEditor/VersionInformation.cs
@@ -0,0 +1,17 @@
+/*Auto-Generated File from VersionEditor 2015.10.20 23:55:53 */
+
+namespace Assets.Scripts.VersionEditor {
+	public static class VersionInformation {
+		public new static string ToString() {
+			return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
+		}
+		public const int Major = 0;
+		public const int Minor = 0;
+		public const int Revision = 0;
+		public const string Build = "1";
+
+		public const string Title = "gravityWell";
+		public const string Company = "borneproud";
+	}
+}
+
