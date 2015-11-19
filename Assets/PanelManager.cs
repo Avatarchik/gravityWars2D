27ac93f5a4546ee054484651becd56f09f7f250a
Ian Mankowski <ltethe@gmail.com>
2015-11-19 00:54:53 -0800

    Option screen created
    Disable gui elements appropriately when option screen is up.

---
+++ Assets/PanelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelManager : MonoBehaviour {
+
+	public GameObject exitPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/scripts/levelSelect.cs
+++ Assets/scripts/levelSelect.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class levelSelect : MonoBehaviour {
+	private GameObject gameManager;
+	private playerState playerStatus;
+
+	private GameObject optionsPanel;
+	private CanvasGroup optionsPanelCanvasGroup;
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public CircleCollider2D player1Collider;
+	public CircleCollider2D player2Collider;
+
+	void Start(){
+		gameManager = GameObject.FindWithTag("gameManager");
+		playerStatus = gameManager.GetComponent<playerState>();
+
+
+		optionsPanel = GameObject.Find("options_panel");
+		optionsPanelCanvasGroup = optionsPanel.GetComponent<CanvasGroup>();
+	}
 
 	public void newLevel()
 	{
@@ -12,4 +33,39 @@ public class levelSelect : MonoBehaviour {
 	{
 		Application.LoadLevel("startScreen");
 	}
+
+	public void hideOptions()
+	{
+		optionsPanelCanvasGroup.alpha = 0f;
+		optionsPanelCanvasGroup.blocksRaycasts = false;	
+
+		player1 = GameObject.FindWithTag("Player1");
+		player1Collider = player1.GetComponent<CircleCollider2D>();
+
+		player2 = GameObject.FindWithTag("Player2");
+		player2Collider = player2.GetComponent<CircleCollider2D>();
+
+		player1Collider.enabled = playerStatus.player1;
+		player2Collider.enabled = playerStatus.player2;
+	
+	}
+
+	public void showOptions()
+	{
+		optionsPanelCanvasGroup.alpha = 1f;
+		optionsPanelCanvasGroup.blocksRaycasts = true;
+
+		player1 = GameObject.FindWithTag("Player1");
+		player1Collider = player1.GetComponent<CircleCollider2D>();
+
+		player2 = GameObject.FindWithTag("Player2");
+		player2Collider = player2.GetComponent<CircleCollider2D>();
+
+		player1Collider.enabled = false;
+		player2Collider.enabled = false;
+	}
+
+	
+
+
 }
