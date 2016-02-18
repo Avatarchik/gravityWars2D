7d37d2c004bcf3d091f2a1828e8cd6d7a7bda76a
Ian Mankowski <ltethe@gmail.com>
2016-02-17 21:56:34 -0800

    goal skimming added

--- Assets/scripts/PlanetSkimDetection.cs
+++ Assets/scripts/PlanetSkimDetection.cs
@@ -11,7 +11,7 @@ public class PlanetSkimDetection : MonoBehaviour {
 	public bool initializeCollider = false;
 	public float waitTime = .25F;
 
-	public enum colliderType{planet, ship};
+	public enum colliderType{planet, ship, goal};
 	public colliderType _colliderType;
 
 	private UnityAction someListener;
@@ -28,41 +28,39 @@ public class PlanetSkimDetection : MonoBehaviour {
 			else{
 				_changeMessage.ChangeText("Close Call!");
 				_score.UpdateScore(30);
-			}	
+			}
 		}
+		else if(_colliderType == colliderType.goal){
+			_changeMessage.ChangeText("This Close!");
+			_score.UpdateScore(30);
+		}	
 	}
 
 	void Start(){
 		sceneManager = GameObject.FindWithTag("gameManager");
 		_score = sceneManager.GetComponent<Score>();
 		_changeMessage = GameObject.Find("message_text").GetComponent<ChangeMessage>();
+
 	}
 
 
 
-
+	//MESSAGING
 	void Awake ()
     {
-        someListener = new UnityAction (playState);
+        someListener = new UnityAction (WaitState);
     }
 
     void OnEnable ()
     {
-        EventManager.StartListening ("playState", someListener);
         EventManager.StartListening ("waitState", WaitState);
     }
 
     void OnDisable ()
     {
-        EventManager.StopListening ("playState", someListener);
         EventManager.StopListening ("waitState", WaitState);
     }
 
-    void playState()
-    {
-    	Debug.Log("playing");
-    }
-    
     void WaitState ()
     {
         initializeCollider = false;
