469f6e924ebcd81061d750558179123af4e71bb2
Ian Mankowski <ltethe@gmail.com>
2016-04-22 00:40:15 -0700

    added touchRing
    Added alpha check scripts to do UI detection based on alpha maps.
    
    touch ring now follows input.

---
+++ Assets/scripts/GUI/DragRotateUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragRotateUI : MonoBehaviour {
+
+	Vector3 startVector;						
+	Vector3 endVector;
+
+	Vector3 firstPosition;
+
+	Quaternion rotation;
+
+	public void BeginDrag()
+	{
+		firstPosition = Input.mousePosition;							//Log the start of the drag
+	}
+
+	public void Drag()
+	{
+		startVector = firstPosition - transform.position;				//Find the first vector
+		endVector = Input.mousePosition - transform.position;			//Find the end vector
+		
+		rotation = Quaternion.FromToRotation(startVector, endVector);	//rotate angle from first vector to end vector
+
+		transform.rotation *= rotation;									//offset rotation transform with our new angle
+
+		firstPosition = Input.mousePosition;							//log the last position as the new start vector
+	}
+
+}
---
+++ Assets/scripts/GUI/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Rotate : MonoBehaviour
+{
+    private const float startAngle = 45;
+    void Update ()
+    {
+        Vector2 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D c = Physics2D.OverlapPoint(mp);
+ 
+        if (c != null && c.gameObject.tag == "Handle" && Input.GetButton("Fire1"))
+        {
+            Vector2 dir = mp - (Vector2)transform.position;
+ 
+            float angle = Mathf.Atan2(dir.y, dir.x)*Mathf.Rad2Deg - startAngle;
+ 
+            if (angle < transform.eulerAngles.z)
+            {
+                transform.eulerAngles = new Vector3(0, 0, angle);
+            }
+        }
+    }
+}
