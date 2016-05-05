a6bb8b705812de7a9504e00f8c2bbcce2c01ed9d
Ian Mankowski <ltethe@gmail.com>
2016-05-05 01:09:38 -0700

    versioning

--- Assets/scripts/VersionEditor/VersionInformation.cs
+++ Assets/scripts/VersionEditor/VersionInformation.cs
@@ -1,4 +1,4 @@
-/*Auto-Generated File from VersionEditor 2016.02.19 17:21:21 */
+/*Auto-Generated File from VersionEditor 2016.05.05 01:09:25 */
 
 namespace Assets.Scripts.VersionEditor {
 	public static class VersionInformation {
@@ -8,7 +8,7 @@ namespace Assets.Scripts.VersionEditor {
 		public const int Major = 0;
 		public const int Minor = 0;
 		public const int Revision = 0;
-		public const string Build = "10";
+		public const string Build = "11";
 
 		public const string Title = "gravityWell";
 		public const string Company = "borneproud";
